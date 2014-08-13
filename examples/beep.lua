8dbe0c101921b5a31430c280569c4a62a9ae5300
Matthew M. Burke <matthew@logicnets.com>
2014-08-13 14:55:40 -0400

    improved example file

--- examples/beep.lua
+++
@@ -1,6 +0,0 @@
---- trial
--- @script trial
-
-function go(d)
-  d:red(); d:sleep(100); d:blue(); d:sleep(100); d:off()
-end
---
+++ examples/toggle.lua
@@ -0,0 +1,19 @@
+--- Toggle the first blink(1) between red and blue. 
+-- @script toggle
+
+local lblink = require 'lblink'
+
+local function doit(d,n)
+  for i = 1,n do
+     d:red(); d:sleep(300); d:blue(); d:sleep(300);
+  end
+  d:off()
+end
+
+
+local d = lblink.open()
+
+if d then
+  doit(d, 4)
+end
+
