bd2924ab5b758300db1bcf2f3cd83786713673f4
Matthew M. Burke <matthew@logicnets.com>
2014-08-13 11:32:18 -0400

    made test (slightly) more sophisticated

--- test/test.lua
+++ test/test.lua
@@ -1,11 +1,40 @@
-#!/usr/bin/env lua -i
+#!/usr/bin/env lua 
+
+-- figure out a better way to do this...
+package.cpath = package.cpath .. ';' .. '../src/?.so'
+
 
 local lblink = require 'lblink'
 
-print('LuaBlink version: ' .. lblink._VERSION)
-print "\n"
-print('VID: ' .. lblink.VID)
-print('PID: ' .. lblink.PID)
-print "\n\n"
+local numericvars = {'VID', 'PID' }
+local stringvars = { '_VERSION' }
+local functions = { 'enumerate', 'list', 'open' }
 
-b = lblink.open()
+
+local function maketester(t)
+   return function(library, name)
+      assert(type(library[name]) == t, name .. ' not defined as a ' .. t .. '.')
+   end
+end
+
+
+local testFunctionDefined = maketester('function')
+local testStringsDefined = maketester('string')
+local testNumericDefined = maketester('number')
+
+
+for _,n in ipairs(numericvars) do
+   testNumericDefined(lblink, n)
+end
+
+
+for _,s in ipairs(stringvars) do
+   testStringsDefined(lblink, s)
+end
+
+
+for _,f in ipairs(functions) do
+   testFunctionDefined(lblink, f)
+end
+
+print "Success"
